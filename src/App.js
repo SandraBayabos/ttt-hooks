63ae072411af7d8fd95c64063cb59249d1aae8ba
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-01-02 12:10:30 +0800

    styled tictactoe

--- src/App.js
+++ src/App.js
@@ -5,30 +5,4 @@ function App() {
   return <div className="App"></div>;
 }
 
-// square
-// value (prop)
-// onclick function (prop)
-
-function Square(props) {
-  return (
-    <button className="Square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-// board
-// state
-// boardState
-// turnState
-// handleclick
-// copy of our boardState
-// if the index of the state of the board is filled, return
-// mutate copy and add X or O
-// calculate next turn
-// set state of the board
-// set state of the turn
-
-// function that calculates the winner
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,121 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square({ value, onClick }) {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Restart({ onClick }) {
+  return (
+    <button className="restart" onClick={onClick}>
+      Play Again
+    </button>
+  );
+}
+
+function Game() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(true);
+  const nextSymbol = isNext ? "X" : "O";
+  const winner = calculateWinner(squares);
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          if (squares[i] != null || winner !== null) {
+            return;
+          }
+          const nextSquares = squares.slice();
+          nextSquares[i] = isNext ? "X" : "O";
+          setSquares(nextSquares);
+          setIsNext(!isNext); // toggle turns
+        }}
+      />
+    );
+  }
+
+  function getStatus() {
+    if (winner) {
+      return "Winner: " + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw!";
+    } else {
+      return "Next player: " + nextSymbol;
+    }
+  }
+
+  function renderRestartButton() {
+    return (
+      <Restart
+        onClick={() => {
+          setSquares(Array(9).fill(null));
+          setIsNext(true);
+        }}
+      />
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="game">
+        <div className="game-board">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+        <div className="game-info">{getStatus()}</div>
+        <div className="restart-button">{renderRestartButton()}</div>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const possibleLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  // go over all possible winning lines & check if they consist of only X's or only O's
+  for (let i = 0; i < possibleLines.length; i++) {
+    const [a, b, c] = possibleLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function isBoardFull(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] == null) {
+      return false;
+    }
+  }
+  return true;
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
