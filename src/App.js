a80ab646a3bf9312fb5ae45a834df6390411deca
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-01-02 11:17:40 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  return <div className="App"></div>;
+}
+
+// square
+// value (prop)
+// onclick function (prop)
+
+function Square(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="Square" onClick={props.onClick}>
+      {props.value}
+    </button>
   );
 }
+
+// board
+// state
+// boardState
+// turnState
+// handleclick
+// copy of our boardState
+// if the index of the state of the board is filled, return
+// mutate copy and add X or O
+// calculate next turn
+// set state of the board
+// set state of the turn
+
+// function that calculates the winner
 
 export default App;
